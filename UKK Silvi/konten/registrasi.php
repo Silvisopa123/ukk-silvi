8c4d2b43aa75e459d3ffbe051454a13a0f1ac045
Silvisopa123 <97864121+Silvisopa123@users.noreply.github.com>
2022-01-27 10:49:10 +0700

    Add files via upload

---
+++ UKK Silvi/konten/registrasi.php
@@ -0,0 +1,35 @@
+<div class="row">
+        <div class="col-md-4 offset-md-4">
+          <div class="card">
+            <div class="card-header">
+              REGISTRASI
+            </div>
+            <div class="card-body">
+              <form action="" method="post">
+                <div class="form-group">
+                  <input type="text" class="form-control" placeholder="NIK"
+                  required autofocus>
+                </div>
+                <div class="form-group">
+                  <input type="text" class="form-control" placeholder="Nama Lengkap"
+                  required>
+                  </div>
+                  <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Username"
+                    required>
+                  </div>
+                    <div class="form-group">
+                      <input type="text" class="form-control" placeholder="Password"
+                      required>
+                    </div>
+                      <div class="form-group">
+                        <label for=""> No.Telpon</label>
+                        <input type="text" class="form-control" placeholder="No.Telpon"
+                        required>
+                      </div>
+                        <button class="btn btn-primary">Kirim</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
